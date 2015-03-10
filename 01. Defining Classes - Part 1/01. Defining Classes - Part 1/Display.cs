028c778a311e8e9acd8c1e6df891a31552a96ee7
vyara <vyara_hristova@yahoo.de>
2015-03-10 23:52:58 +0200

    initial commit

---
+++ 01. Defining Classes - Part 1/01. Defining Classes - Part 1/Display.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+
+class Display
+{
+    //fields
+    private ulong height;
+    private ulong width;
+    private ulong? numberOfColors;
+
+    //constructors
+    public Display(ulong height, ulong width)
+    {
+        this.Height = height;
+        this.Width = width;
+        this.NumberOfColors = null;
+    }
+
+    public Display(ulong height, ulong width, ulong numberOfColors)
+        : this(height, width)
+    {
+        this.NumberOfColors = numberOfColors;
+    }
+
+    //properties
+    public ulong Height
+    {
+        get
+        {
+            return this.height;
+        }
+
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Display height needs to be > 0.");
+            }
+
+            this.height = value;
+        }
+    }
+
+    public ulong Width
+    {
+        get
+        {
+            return this.width;
+        }
+
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Display width needs to be > 0.");
+            }
+
+            this.width = value;
+        }
+    }
+
+    public ulong? NumberOfColors
+    {
+        get
+        {
+            return this.numberOfColors;
+        }
+
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Display number of colors needs to be > 0.");
+            }
+
+            this.numberOfColors = value;
+        }
+    }
+
+}
---
+++ 01. Defining Classes - Part 1/01. Defining Classes - Part 1/GSMTest.cs
@@ -0,0 +1,43 @@
+﻿//Write a class GSMTest to test the GSM class:
+//Create an array of few instances of the GSM class.
+//Display the information about the GSMs in the array.
+//Display the information about the static property IPhone4S.
+
+
+using System;
+
+
+
+
+class Tests
+{
+    static void Main()
+    {
+        Console.WriteLine("GSM instances test");
+        Console.WriteLine(new string('-', 30));
+        //creates array with GSM instances
+        var gsms = new GSM[] 
+        {
+            new GSM("Galaxy S3","Samsung"),
+            new GSM("G2 Mini", "LG", 190, new Battery("2440 mAh", 700, 12, Battery.Type.LiIon), new Display(540, 960, 16000000), "Martin"),
+            new GSM("Nexus 6", "Motorola", new Display(1440, 2560)),
+            new GSM("One M9", "HTC", 500, new Battery("Non-removable 2840 mAh", 391, 25), new Display(1080, 1920, 160000000))
+        };
+
+        //displays the information about the GSMs in the array
+        foreach (var gsm in gsms)
+        {
+            Console.WriteLine(gsm.ToString());
+        }
+
+        //displays the information about the static property IPhone4S
+        Console.WriteLine("Information about iPhone 4S: ");
+        Console.WriteLine(new string('-', 30));
+        Console.WriteLine(GSM.IPhone4S);
+
+        //tests calls
+        GSMCallHistoryTest.CallHistoryTest();
+    }
+
+}
+
